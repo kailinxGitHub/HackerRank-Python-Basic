49ebd2d9e65719a3a074181d86f9f8a14a105756
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-07 17:19:32 +0200

    updated

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-list = []
-command = str(input())
-split_command = command.split()
-if split_command[0] == "insert":
-    vari = int(split_command[1])
-    vare = int(split_command[2])
-    list.insert(vari, vare)
-    print(list)
+def split_and_join(line):
+    words = line.split()
+    joinedwords = '-'.join(words)
+    return joinedwords
+
+
+line = "this is a string"
+result = split_and_join(line)
+print(result)
