e308d979ff86451b4069e6fdc8b797f8bee8451f
kk-clone <kailinx@kailinx.com>
2023-04-07 17:44:14 +0200

    update

--- 04072024easy.py
+++ 04072024easy.py
@@ -122,5 +122,26 @@ if __name__ == '__main__':
     print(s_new)
 
 #Find a String
+def count_substring(string, sub_string):
+    innercount = 0
+    sunique = set()
+    newstring = [i for i in string if i not in sunique and not sunique.add(i)]
+    ssunique = set()
+    newsub_string = [j for j in sub_string if j not in ssunique and not ssunique.add(j)]
+    for k in newstring:
+        for z in newsub_string:
+            if k == z:
+                innercount += 1
+    return innercount
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
+
+
+
 
 
