a64eb7a2dc6217f3e66f8a1971d4c2a93fc75538
kk-clone <kailinx@kailinx.com>
2023-04-07 16:21:27 +0200

    commit

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-x=2
-y=2
-z=2
-n=2
-xlist = [i for i in range(x+1)]
-ylist = [j for j in range(y+1)]
-zlist = [k for k in range(z+1)]
-perm = [(i, j, k) for i in xlist for j in ylist for k in zlist if i+j+k<n]
-print(perm)
+list = []
+command = str(input())
+split_command = command.split()
+if split_command[0] == "insert":
+    vari = int(split_command[1])
+    vare = int(split_command[2])
+    list.insert(vari, vare)
+    print(list)
