665efce4c2749571690ecfbd3c2cfdf94652ca59
kk-clone <kailinx@kailinx.com>
2023-04-08 18:53:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(100%4)
+def print_formatted(number):
+    if n in range(1, 100):
+        for i in range(1,n+1):
+            innerlist = [i, oct(i)[2:], hex(i).upper()[2:], bin(i)[2:]]
+            print("  ".join(map(str, innerlist)))
+        
+
+
+n = 2
+print_formatted(n)
