2d2c53e84215336d0d8c094e088b53f651b94dcd
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-08 18:53:02 +0200

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -76,3 +76,79 @@ if __name__ == '__main__':
     print(any([i.isupper() for i in s]))
 
 #Text Alignment
+thickness = int(input()) #This must be an odd number
+c = 'H'
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+#Text Wrap
+import textwrap
+def wrap(string, max_width):
+    outlist = []
+    for i in range(0,len(string), max_width):
+        for j in range(max_width+1):
+            s = string[i:i+j]
+            if j == max_width:
+                outlist.append(s)
+            else: 
+                continue
+    return "\n".join(outlist)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+
+#Designer Door Mat
+
+#String Formatting
+def print_formatted(number):
+    w=len(str(bin(number))[2:])
+    if number in range(1, 100):
+        for i in range(1,number+1):
+            print(str(i).rjust(w,' '),oct(i)[2:].rjust(w,' '),hex(i)[2:].upper().rjust(w,' '),bin(i)[2:].rjust(w,' '))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
+
+#Alphabet Rangolo
+
+
+#Capitalize!
+import math
+import os
+import random
+import re
+import sys
+def solve(s):
+    return " ".join([x.capitalize() for x in s.split(" ")])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+#The Minion Game
+
+#Merge the Tools!
+
