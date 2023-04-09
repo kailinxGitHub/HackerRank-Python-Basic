dc6c766eaf5f8af597e94d956de7e13504186cf1
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-09 17:51:30 +0200

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -36,32 +36,75 @@ def add():
 add()
 
 #Set .discard(), .remove() & .pop()
-n = int(input())
-s = set(map(int, input().split()))
-N = int(input())
-if n in range(0, 20) and N in range(0, 20):
-    for i in range(N):
-        command = str(input()).split(" ")
-        if command[0] == "pop":
-            s.pop()
-        elif command[0] == "remove":
-            s.remove(int(command[1]))
-        elif command[0] == "discard":
-            s.discard(int(command[1]))
-    print(sum(s))
+def setactions(): 
+    n = int(input())
+    s = set(map(int, input().split()))
+    N = int(input())
+    if n in range(0, 20) and N in range(0, 20):
+        for i in range(N):
+            command = str(input()).split(" ")
+            if command[0] == "pop":
+                s.pop()
+            elif command[0] == "remove":
+                s.remove(int(command[1]))
+            elif command[0] == "discard":
+                s.discard(int(command[1]))
+        return sum(s)
 
 #Set .union() Operation
-
+n = int(input())
+a = set(str(input()).split(" "))
+b = int(input())
+c = set(str(input()).split(" "))
+y = set(a.union(c))
+print(len(y))
 
 #Set .intersection() Operation
+n = int(input())
+a = set(str(input()).split(" "))
+b = int(input())
+c = set(str(input()).split(" "))
+y = set(a.intersection(c))
+print(len(y))
 
 #Set .difference() Operation
+n = int(input())
+a = set(str(input()).split(" "))
+b = int(input())
+c = set(str(input()).split(" "))
+y = set(a.difference(c))
+print(len(y))
 
 #Set .symmetric_difference() Operation
+n = int(input())
+a = set(str(input()).split(" "))
+b = int(input())
+c = set(str(input()).split(" "))
+y = set(a.symmetric_difference(c))
+print(len(y))
 
 #Set Mutations
+A = int(input())
+Aset = set(map(int, input().split()))
+N = int(input())
+
+if len(set(Aset)) in range(0, 1000) and N in range(0, 100):
+    for i in range(N):
+        command = input().split()
+        Bset = set(map(int, input().split()))
+        if len(Bset) in range(0, 100):
+            if command[0] == "update":
+                Aset.update(Bset)
+            elif command[0] == "intersection_update":
+                Aset.intersection_update(Bset)
+            elif command[0] == "difference_update":
+                Aset.difference_update(Bset)
+            elif command[0] == "symmetric_difference_update":
+                Aset.symmetric_difference_update(Bset)
+    print(sum(Aset))
 
 #The Captain's Room
+
 
 #Check Subset
 
