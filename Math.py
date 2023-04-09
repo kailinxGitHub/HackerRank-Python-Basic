abd561f7b3b3db096fe192cdf8d65944f52f6f34
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-09 21:29:55 +0200

    Update Math.py

---
+++ Math.py
@@ -0,0 +1,23 @@
+#Polar Coordinates
+import cmath
+
+def polar_coordinates(z):
+    print(abs(complex(z)))
+    print(cmath.phase(complex(z)))
+
+z = input()
+polar_coordinates(z)
+
+#Find Angle MBC
+
+
+#Triangle Quest 2
+
+#Mod Divmod
+
+#Power - Mod Power
+
+#Integers Come In All Sizes
+
+#Triangle Quest
+
