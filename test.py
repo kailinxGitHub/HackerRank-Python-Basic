3808f00ee2af08aa8290c2fab212cb455648c0fb
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-09 10:29:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-def print_formatted(number):
-    if n in range(1, 100):
-        for i in range(1,n+1):
-            innerlist = [i, oct(i)[2:], hex(i).upper()[2:], bin(i)[2:]]
-            print("  ".join(map(str, innerlist)))
-        
+def average(array):
+    if n in range(0, 101):
+        array = set(arr)
+        return sum(array)/len(array)
 
 
-n = 2
-print_formatted(n)
+n = 10
+arr = list([161, 182, 161, 154, 176, 170, 167, 171, 170, 174])
+result = average(arr)
+print(result)
