85e87f41f5ad0e9e4ccc3f25473cdf0474210491
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-09 21:49:59 +0200

    Update Math.py

--- Math.py
+++ Math.py
@@ -9,9 +9,17 @@ z = input()
 polar_coordinates(z)
 
 #Find Angle MBC
+import math
+def find_angle_mbc(line1, line2):
+    if (line1 and line2) in range(0, 101):
+        print(f"{round(math.degrees(math.atan(ab/bc)))}\N{DEGREE SIGN}")
 
+ab = float(input())
+bc = float(input())
+find_angle_mbc(ab, bc)
 
 #Triangle Quest 2
+
 
 #Mod Divmod
 
