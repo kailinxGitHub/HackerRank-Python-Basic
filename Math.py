ad5f3a9b198cbb0fa70f270041a3737ea3f6ce30
kk-clone <kailinx@kailinx.com>
2023-04-09 22:04:01 +0200

    Update Math.py

--- Math.py
+++ Math.py
@@ -19,13 +19,35 @@ bc = float(input())
 find_angle_mbc(ab, bc)
 
 #Triangle Quest 2
-
+for i in range(1,int(input())+1): 
+    print(((10**i)//9)**2)
 
 #Mod Divmod
+a = int(input())
+b = int(input())
+
+print(divmod(a,b)[0])
+print(divmod(a,b)[1])
+print(divmod(a,b))
 
 #Power - Mod Power
+a = int(input())
+b = int(input())
+m = int(input())
+
+if (a and b) in range(1, 11) and m in range(2, 1001):
+    print(pow(a,b))
+    print(pow(a,b,m))
 
 #Integers Come In All Sizes
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if (a and b and c and d) in range(1, 1001): 
+    print(a**b+c**d)
 
 #Triangle Quest
-
+for i in range(1,int(input())):
+    print((10**i)//9*i)
