fea44ff22bd18295b8e579e11991c2aeaf82d371
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-09 10:29:46 +0200

    Update Sets.py

---
+++ Sets.py
@@ -0,0 +1,33 @@
+#Introduction to Sets
+def average(array):
+    if n in range(0, 101):
+        array = set(arr)
+        return sum(array)/len(array)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+
+#Symmetric Difference
+
+#Set .add()
+
+#Set .discard(), .remove() & .pop()
+
+#Set .union() Operation
+
+#Set .intersection() Operation
+
+#Set .difference() Operation
+
+#Set .symmetric_difference() Operation
+
+#Set Mutations
+
+#The Captain's Room
+
+#Check Subset
+
+#Check Strict Superset
