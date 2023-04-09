17a61899101bed4d1a7186ec80ebb633e1f1743e
kk-clone <kailinx@kailinx.com>
2023-04-09 13:01:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-def average(array):
-    if n in range(0, 101):
-        array = set(arr)
-        return sum(array)/len(array)
+def add():
+    n = int(input())
+    myset = set()
+    if n in range(0, 1000):
+        for i in range(n):
+            myset.add(str(input()))
+    print(len(myset))
 
-
-n = 10
-arr = list([161, 182, 161, 154, 176, 170, 167, 171, 170, 174])
-result = average(arr)
-print(result)
+add()
