4b7df0eb7a2b83563b3893989e88135a30c9b298
Kailin <kailinx@kailinx.com>
2023-04-20 20:15:49 +0200

    Update Itertools.py

--- Itertools.py
+++ Itertools.py
@@ -1,5 +1,18 @@
 #itertools.product()
+from itertools import product
 
+inputA = input()
+listA = inputA.split()
+A = [int(i) for i in listA]
+
+inputB = input()
+listB = inputB.split()
+B = [int(j) for j in listB]
+
+prod = list(product(A,B))
+
+for t in prod:
+    print(t, end=" ")
 
 #itertools.permutations()
 
