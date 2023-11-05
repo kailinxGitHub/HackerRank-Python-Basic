ac6ac6e431be3fdb29d5b5a04cafadfd634dad95
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-11-05 01:20:17 +0000

    updated

--- test.py
+++
@@ -1,9 +0,0 @@
-from itertools import combinations
-
-string = "HACK 2"
-list1 = [i for i in string if i != " "][:-1]
-number = list[-1]
-# print(list(combinations(list1[:-1],int(number[-1]))))
-# for i in list(combinations(list1,1)).sort(): print(i)
-# for i in list(combinations(list1,2)).sort(): print(i)
-print(list(combinations(list1,1)))
